1165ee677ba0274bc14f7ffb2fc0620bc2a9a3a1
padrewin <97427230+padrewin@users.noreply.github.com>
2025-04-26 19:40:59 +0300

    Update GiveAll.java

--- src/main/java/dev/padrewin/giveall/GiveAll.java
+++ src/main/java/dev/padrewin/giveall/GiveAll.java
@@ -4,6 +4,8 @@ import dev.padrewin.colddev.ColdPlugin;
 import dev.padrewin.colddev.config.ColdSetting;
 import dev.padrewin.colddev.manager.Manager;
 import dev.padrewin.colddev.manager.PluginUpdateManager;
+import dev.padrewin.giveall.hook.GiveAllPlaceholderExpansion;
+import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import dev.padrewin.giveall.manager.CommandManager;
@@ -13,9 +15,25 @@ import dev.padrewin.giveall.setting.SettingKey;
 import java.io.File;
 import java.util.List;
 
+import static dev.padrewin.colddev.manager.AbstractDataManager.ANSI_BOLD;
+import static dev.padrewin.colddev.manager.AbstractDataManager.ANSI_LIGHT_BLUE;
+
 public final class GiveAll extends ColdPlugin {
 
+    /**
+     * Console colors
+     */
+    String ANSI_RESET = "\u001B[0m";
+    String ANSI_CHINESE_PURPLE = "\u001B[38;5;93m";
+    String ANSI_PURPLE = "\u001B[35m";
+    String ANSI_GREEN = "\u001B[32m";
+    String ANSI_RED = "\u001B[31m";
+    String ANSI_AQUA = "\u001B[36m";
+    String ANSI_PINK = "\u001B[35m";
+    String ANSI_YELLOW = "\u001B[33m";
+
     private static GiveAll instance;
+    private GiveAllPlaceholderExpansion placeholderExpansion;
 
     public GiveAll() {
         super("Cold-Development", "GiveAll", 23387, null, LocaleManager.class, null);
@@ -27,18 +45,27 @@ public final class GiveAll extends ColdPlugin {
         instance = this;
         getManager(PluginUpdateManager.class);
 
+        // Initialize PlaceholderAPI Hook
+        if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            this.placeholderExpansion = new GiveAllPlaceholderExpansion(this);
+            this.placeholderExpansion.register();
+            getLogger().info(ANSI_LIGHT_BLUE + "PlaceholderAPI hook registered successfully. " + ANSI_BOLD + ANSI_GREEN + "✔" + ANSI_RESET);
+        } else {
+            this.placeholderExpansion = null;
+            getLogger().warning(ANSI_LIGHT_BLUE + "PlaceholderAPI not found. " + ANSI_BOLD + ANSI_RED + "✘" + ANSI_RESET);
+        }
+
         String name = getDescription().getName();
         getLogger().info("");
-        getLogger().info("  ____ ___  _     ____  ");
-        getLogger().info(" / ___/ _ \\| |   |  _ \\ ");
-        getLogger().info("| |  | | | | |   | | | |");
-        getLogger().info("| |__| |_| | |___| |_| |");
-        getLogger().info(" \\____\\___/|_____|____/");
-        getLogger().info("    " + name + " v" + getDescription().getVersion());
-        getLogger().info("    Author(s): " + (String)getDescription().getAuthors().get(0));
-        getLogger().info("    (c) Cold Development ❄");
+        getLogger().info(ANSI_CHINESE_PURPLE + "  ____ ___  _     ____  " + ANSI_RESET);
+        getLogger().info(ANSI_PINK + " / ___/ _ \\| |   |  _ \\ " + ANSI_RESET);
+        getLogger().info(ANSI_CHINESE_PURPLE + "| |  | | | | |   | | | |" + ANSI_RESET);
+        getLogger().info(ANSI_PINK + "| |__| |_| | |___| |_| |" + ANSI_RESET);
+        getLogger().info(ANSI_CHINESE_PURPLE + " \\____\\___/|_____|____/ " + ANSI_RESET);
+        getLogger().info("    " + ANSI_GREEN + name + ANSI_RED + " v" + getDescription().getVersion() + ANSI_RESET);
+        getLogger().info(ANSI_PURPLE + "    Author(s): " + ANSI_PURPLE + getDescription().getAuthors().get(0) + ANSI_RESET);
+        getLogger().info(ANSI_AQUA + "    (c) Cold Development ❄" + ANSI_RESET);
         getLogger().info("");
-
 
         File configFile = new File(getDataFolder(), "en_US.yml");
         if (!configFile.exists()) {
@@ -48,7 +75,9 @@ public final class GiveAll extends ColdPlugin {
 
     @Override
     public void disable() {
-        getLogger().info("Plugin unloaded.");
+        getLogger().info("");
+        getLogger().info(ANSI_CHINESE_PURPLE + "GiveAll disabled." + ANSI_RESET);
+        getLogger().info("");
     }
 
     @Override
@@ -66,12 +95,13 @@ public final class GiveAll extends ColdPlugin {
     @Override
     protected String[] getColdConfigHeader() {
         return new String[] {
-                "  ____  ___   _      ____   ",
-                " / ___|/ _ \\ | |    |  _ \\  ",
-                "| |   | | | || |    | | | | ",
-                "| |___| |_| || |___ | |_| | ",
-                " \\____|\\___/ |_____|_____/  ",
-                "                           "
+                " ██████╗ ██████╗ ██╗     ██████╗ ",
+                "██╔════╝██╔═══██╗██║     ██╔══██╗",
+                "██║     ██║   ██║██║     ██║  ██║",
+                "██║     ██║   ██║██║     ██║  ██║",
+                "╚██████╗╚██████╔╝███████╗██████╔╝",
+                " ╚═════╝ ╚═════╝ ╚══════╝╚═════╝ ",
+                "                                 "
         };
     }
 
@@ -89,5 +119,10 @@ public final class GiveAll extends ColdPlugin {
         }
         return item.getType().toString().replace("_", " ").toLowerCase();
     }
+
+    public GiveAllPlaceholderExpansion getPlaceholderExpansion() {
+        return placeholderExpansion;
+    }
+
 
 }
